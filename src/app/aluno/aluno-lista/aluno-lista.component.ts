4c49b9933f04e7697b38075270a9696749eb8c46
walkerrabelo <walkerrabelo@gmail.com>
2019-07-18 13:35:20 -0300

    Add example data to List

--- src/app/aluno/aluno-lista/aluno-lista.component.ts
+++ src/app/aluno/aluno-lista/aluno-lista.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Aluno } from '../../models/aluno/aluno';
+import { LISTA_ALUNOS_EXEMPLO } from './alunos-lista-exemplo';
 
 @Component({
   selector: 'app-aluno-lista',
   templateUrl: './aluno-lista.component.html',
   styleUrls: ['./aluno-lista.component.css']
 })
+
 export class AlunoListaComponent implements OnInit {
+
+  listaAlunos = LISTA_ALUNOS_EXEMPLO;
 
   constructor() { }
 
---
+++ src/app/aluno/aluno-lista/alunos-lista-exemplo.ts
@@ -0,0 +1,61 @@
+import { Aluno } from '../../models/aluno/aluno';
+export const LISTA_ALUNOS_EXEMPLO: Aluno[] = [
+    {
+      id_aluno: '1',
+      nome: 'Fulano de Tal dos Anzóis',
+      email: 'fulanodetaldosanzois@gmail.com',
+      data_nascimento: '2019-06-06',
+      data_matricula: '2019-06-06',
+      sexo: 'Masculino',
+      grupo: '0',
+      id_professor: '18',
+      id_usuario: '30814',
+      id_iugu: null,
+      hash: '3AhyhFrAJShxyNpFFDO9biDjst9w9Sew0mBC2N62iArzh4rRy',
+      ativo: '1',
+      visivel: '1',
+      usuario: {
+        id_usuario: '1',
+        nome: 'Fulano de Tal dos Anzóis',
+        login: 'fulanodetaldosanzois',
+        senha: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+        email: 'fulanodetaldosanzois@gmail.com',
+        hash: 'h6uNSU09AQVnyS70nIqCQK2HXWMWdnVNkEmvSxEre0f3lrgy',
+        ativo: '1',
+        player_id: '',
+        id_tipo_usuario: '3',
+        data_matricula: '2019-06-06',
+        link_foto: null,
+        ultimo_login: null
+      }
+    },
+    {
+      id_aluno: '2',
+      nome: 'Maria de Tal dos Anzóis',
+      email: 'mariadetaldosanzois@gmail.com',
+      data_nascimento: '2019-06-06',
+      data_matricula: '2019-06-06',
+      sexo: 'Feminino',
+      grupo: '0',
+      id_professor: '18',
+      id_usuario: '30814',
+      id_iugu: null,
+      hash: '3AhyhFrAJShxyNpFFDO9biDjst9w9Sew0mBC2N62iArzh4rRy',
+      ativo: '1',
+      visivel: '1',
+      usuario: {
+        id_usuario: '2',
+        nome: 'Maria de Tal dos Anzóis',
+        login: 'mariadetal',
+        senha: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+        email: 'mariadetaldosanzois@gmail.com',
+        hash: 'h6uNSU09AQVnyS70nIqCQK2HXWMWdnVNkEmvSxEre0f3lrgy',
+        ativo: '1',
+        player_id: '',
+        id_tipo_usuario: '3',
+        data_matricula: '2019-06-06',
+        link_foto: null,
+        ultimo_login: null
+      }
+    },
+];

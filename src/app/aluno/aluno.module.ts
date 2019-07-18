dc68a1ba723e008776b5dd1ff0e946ff40aeaf4b
walkerrabelo <walkerrabelo@gmail.com>
2019-07-18 10:48:31 -0300

    Add message bag

--- src/app/aluno/aluno.module.ts
+++ src/app/aluno/aluno.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlunoAvaliacaoComponent } from './aluno-avaliacao/aluno-avaliacao.component';
 import { MatIconModule, MatToolbarModule, MatDividerModule, MatTabsModule, MatTooltipModule,
-  MatDialogModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatRadioModule } from '@angular/material';
+  MatDialogModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatBadgeModule } from '@angular/material';
 import { AlunoComponent } from './aluno/aluno.component';
 import { AlunoListaComponent } from './aluno-lista/aluno-lista.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
@@ -37,6 +37,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatTooltipModule,
     MatRadioModule,
+    MatBadgeModule,
 
     AlunoTreinoModule,
   ],

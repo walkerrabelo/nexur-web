441b8c3198b7d4195284907a2ff05d6e288bdafc
walkerrabelo <walkerrabelo@gmail.com>
2019-07-23 12:44:09 -0300

    Add url image

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   ambiente: 'produção',
-  api_url: 'https://api.nexur.com.br'
+  api_url: 'https://api.nexur.com.br',
+  exercicios_url: 'https://fit.nexur.com.br/exercicios'
 };

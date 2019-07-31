06980854240b01d25d10478e725f5bfd2bb4390c
walkerrabelo <walkerrabelo@gmail.com>
2019-07-31 17:33:51 -0300

    Add nexur logo to mat tool bar

--- src/app/shared/pipes/route-friendly.pipe.ts
+++ src/app/shared/pipes/route-friendly.pipe.ts
@@ -2,14 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 
 const ROUTES_NEXUR = new Map([
-  ['/home', '/ Home'],
-  ['/alunos', '/ Lista de Alunos'],
-  ['/aluno', '/ Aluno'],
-  ['/aulas', '/ Calendário de Aulas'],
-  ['/financeiro', '/ Financeiro'],
-  ['/exercicios', '/ Exercícios'],
-  ['/avaliacoes', '/ Modelos de Avaliacoes'],
-  ['/preferencias', '/ Preferências']
+  ['/home', 'Home'],
+  ['/alunos', 'Lista de Alunos'],
+  ['/aluno', 'Aluno'],
+  ['/aulas', 'Calendário de Aulas'],
+  ['/financeiro', 'Financeiro'],
+  ['/exercicios', 'Exercícios'],
+  ['/avaliacoes', 'Modelos de Avaliacoes'],
+  ['/preferencias', 'Preferências']
 ]);
 
 @Pipe({
